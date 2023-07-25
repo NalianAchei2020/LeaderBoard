805a2b0865a7131bb2ca7e7e32aa422a867e3d99
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-07-25 09:28:29 +0100

    add list items

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import { addData, displayData } from './modules/addData.js';
 import './style.css';
-
-const button = document.querySelector('#submit');
-button.addEventListener('click', addData);
-
-window.addEventListener('load', displayData);
